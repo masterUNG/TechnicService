17aa209fc79037b2f71c21426ab3f0ddde4971f0
ung <test@abc.com>
2022-12-06 11:52:22 +0700

    Register Success

--- lib/states/create_account_technic.dart
+++ lib/states/create_account_technic.dart
@@ -95,163 +95,166 @@ class _CreateAccountTeachnicState extends State<CreateAccountTeachnic> {
                     width: boxConstraints.maxWidth,
                     height: boxConstraints.maxHeight -
                         boxConstraints.maxWidth * 0.4,
-                    child: ListView(
-                      children: [
-                        WidgetText(
-                          text: 'ข้อมูลช่าง :',
-                          textStyle: AppConstant().h2Style(),
-                        ),
-                        WidgetForm(
-                          labelWidget: const WidgetText(text: 'ชื่อ'),
-                          changeFunc: (p0) {
-                            name = p0.trim();
-                          },
-                        ),
-                        WidgetForm(
-                          labelWidget: const WidgetText(text: 'นามสกุล'),
-                          changeFunc: (p0) {
-                            surName = p0.trim();
-                          },
-                        ),
-                        WidgetForm(
-                          labelWidget: const WidgetText(text: 'ที่อยู่'),
-                          changeFunc: (p0) {
-                            address = p0.trim();
-                          },
-                        ),
-                        WidgetForm(
-                          textInputType: TextInputType.phone,
-                          labelWidget: const WidgetText(text: 'เบอร์โทรศัพย์'),
-                          changeFunc: (p0) {
-                            phone = p0.trim();
-                          },
-                        ),
-                        WidgetForm(
-                          textInputType: TextInputType.emailAddress,
-                          labelWidget: const WidgetText(text: 'Email'),
-                          changeFunc: (p0) {
-                            email = p0.trim();
-                          },
-                        ),
-                        WidgetForm(
-                          labelWidget: const WidgetText(text: 'Password'),
-                          changeFunc: (p0) {
-                            password = p0.trim();
-                          },
-                        ),
-                        Container(
-                          margin: const EdgeInsets.symmetric(vertical: 16),
-                          child: WidgetText(
-                            text: 'ชนิดของช่าง :',
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          WidgetText(
+                            text: 'ข้อมูลช่าง :',
                             textStyle: AppConstant().h2Style(),
                           ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          decoration: AppConstant().borderCurveBox(),
-                          width: boxConstraints.maxWidth,
-                          child: typeTeachnicModels.isEmpty
-                              ? const WidgetProgress()
-                              : ListView.builder(
-                                  shrinkWrap: true,
-                                  physics: const ScrollPhysics(),
-                                  itemCount: typeTeachnicModels.length,
-                                  itemBuilder: (context, index) {
-                                    return CheckboxListTile(
-                                      controlAffinity:
-                                          ListTileControlAffinity.leading,
-                                      value: chooseTypeTechnics[index],
-                                      onChanged: (value) {
-                                        chooseTypeTechnics[index] = value!;
-                                        setState(() {});
-                                      },
-                                      title: WidgetText(
-                                          text: typeTeachnicModels[index].name),
-                                    );
-                                  },
-                                ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.symmetric(vertical: 16),
-                          child: WidgetText(
-                            text: 'พิกัด :',
-                            textStyle: AppConstant().h2Style(),
+                          WidgetForm(
+                            labelWidget: const WidgetText(text: 'ชื่อ'),
+                            changeFunc: (p0) {
+                              name = p0.trim();
+                            },
                           ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(4),
-                          decoration: AppConstant().borderCurveBox(),
-                          width: boxConstraints.maxWidth * 0.8,
-                          height: boxConstraints.maxWidth * 0.6,
-                          child: position == null
-                              ? const WidgetProgress()
-                              : WidgetGoogleMap(
-                                  lat: position!.latitude,
-                                  lng: position!.longitude),
-                        ),
-                        WidgetButton(
-                          label: 'ยืนยัน',
-                          pressFunc: () {
-                            print('name = $name, surname = $surName');
-
-                              AppDialog(context: context).materialDialog();
-
-                            // if ((name?.isEmpty ?? true) ||
-                            //     (surName?.isEmpty ?? true) ||
-                            //     (address?.isEmpty ?? true) ||
-                            //     (phone?.isEmpty ?? true) ||
-                            //     (email?.isEmpty ?? true) ||
-                            //     (password?.isEmpty ?? true)) {
-                            //   // AppDialog(context: context).normalDialog(
-                            //   //     title: 'มีช่องว่าง ?',
-                            //   //     detail: 'กรุณากรอกให้ ครบทุกช่อง ครับ');
-                            //   AppDialog(context: context).materialDialog();
-                            // } else if (AppService().checkChooseTypeTechnic(
-                            //     listChooses: chooseTypeTechnics)) {
-                            //   AppDialog(context: context).normalDialog(
-                            //       title: 'ยังไม่ได้เลือก ชนิดช่าง',
-                            //       detail: 'กรุณาเลือก ชนิกของช่าง');
-                            // } else {
-                            //   print(
-                            //       'chooseTypeTechnic ---> $chooseTypeTechnics');
-
-                            //   var skillTechnics = <String>[];
-
-                            //   for (var i = 0;
-                            //       i < chooseTypeTechnics.length;
-                            //       i++) {
-                            //     if (chooseTypeTechnics[i]) {
-                            //       skillTechnics.add(typeTeachnicModels[i].name);
-                            //     }
-                            //   }
-                            //   print('skillTechnics --> $skillTechnics');
-
-                            //   UserModel userModel = UserModel(
-                            //     name: name!,
-                            //     surName: surName!,
-                            //     address: address!,
-                            //     phone: phone!,
-                            //     email: email!,
-                            //     password: password!,
-                            //     typeUser: AppConstant.typeUsers[1],
-                            //     geoPoint: GeoPoint(
-                            //         position!.latitude, position!.longitude),
-                            //     skillTechnic: skillTechnics,
-                            //   );
-
-                            //   AppService()
-                            //       .processCreateNewAccount(
-                            //           email: email!,
-                            //           password: password!,
-                            //           context: context,
-                            //           userModel: userModel)
-                            //       .then((value) {
-                            //     // Get.offAllNamed(AppConstant.pageMainHome);
-                            //   });
-                            // }
-                          }, // onpresss
-                        ),
-                      ],
+                          WidgetForm(
+                            labelWidget: const WidgetText(text: 'นามสกุล'),
+                            changeFunc: (p0) {
+                              surName = p0.trim();
+                            },
+                          ),
+                          WidgetForm(
+                            labelWidget: const WidgetText(text: 'ที่อยู่'),
+                            changeFunc: (p0) {
+                              address = p0.trim();
+                            },
+                          ),
+                          WidgetForm(
+                            textInputType: TextInputType.phone,
+                            labelWidget: const WidgetText(text: 'เบอร์โทรศัพย์'),
+                            changeFunc: (p0) {
+                              phone = p0.trim();
+                            },
+                          ),
+                          WidgetForm(
+                            textInputType: TextInputType.emailAddress,
+                            labelWidget: const WidgetText(text: 'Email'),
+                            changeFunc: (p0) {
+                              email = p0.trim();
+                            },
+                          ),
+                          WidgetForm(
+                            labelWidget: const WidgetText(text: 'Password'),
+                            changeFunc: (p0) {
+                              password = p0.trim();
+                            },
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 16),
+                            child: WidgetText(
+                              text: 'ชนิดของช่าง :',
+                              textStyle: AppConstant().h2Style(),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: AppConstant().borderCurveBox(),
+                            width: boxConstraints.maxWidth,
+                            child: typeTeachnicModels.isEmpty
+                                ? const WidgetProgress()
+                                : ListView.builder(
+                                    shrinkWrap: true,
+                                    physics: const ScrollPhysics(),
+                                    itemCount: typeTeachnicModels.length,
+                                    itemBuilder: (context, index) {
+                                      return CheckboxListTile(
+                                        controlAffinity:
+                                            ListTileControlAffinity.leading,
+                                        value: chooseTypeTechnics[index],
+                                        onChanged: (value) {
+                                          chooseTypeTechnics[index] = value!;
+                                          setState(() {});
+                                        },
+                                        title: WidgetText(
+                                            text: typeTeachnicModels[index].name),
+                                      );
+                                    },
+                                  ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 16),
+                            child: WidgetText(
+                              text: 'พิกัด :',
+                              textStyle: AppConstant().h2Style(),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(4),
+                            decoration: AppConstant().borderCurveBox(),
+                            width: boxConstraints.maxWidth * 0.8,
+                            height: boxConstraints.maxWidth * 0.6,
+                            child: position == null
+                                ? const WidgetProgress()
+                                : WidgetGoogleMap(
+                                    lat: position!.latitude,
+                                    lng: position!.longitude),
+                          ),
+                          WidgetButton(
+                            label: 'ยืนยัน',
+                            pressFunc: () {
+                              print('name = $name, surname = $surName');
+                      
+                                // AppDialog(context: context).materialDialog();
+                      
+                              if ((name?.isEmpty ?? true) ||
+                                  (surName?.isEmpty ?? true) ||
+                                  (address?.isEmpty ?? true) ||
+                                  (phone?.isEmpty ?? true) ||
+                                  (email?.isEmpty ?? true) ||
+                                  (password?.isEmpty ?? true)) {
+                                AppDialog(context: context).normalDialog(
+                                    title: 'มีช่องว่าง ?',
+                                    detail: 'กรุณากรอกให้ ครบทุกช่อง ครับ');
+                               
+                              } else if (AppService().checkChooseTypeTechnic(
+                                  listChooses: chooseTypeTechnics)) {
+                                AppDialog(context: context).normalDialog(
+                                    title: 'ยังไม่ได้เลือก ชนิดช่าง',
+                                    detail: 'กรุณาเลือก ชนิกของช่าง');
+                              } else {
+                                print(
+                                    'chooseTypeTechnic ---> $chooseTypeTechnics');
+                      
+                                var skillTechnics = <String>[];
+                      
+                                for (var i = 0;
+                                    i < chooseTypeTechnics.length;
+                                    i++) {
+                                  if (chooseTypeTechnics[i]) {
+                                    skillTechnics.add(typeTeachnicModels[i].name);
+                                  }
+                                }
+                                print('skillTechnics --> $skillTechnics');
+                      
+                                UserModel userModel = UserModel(
+                                  name: name!,
+                                  surName: surName!,
+                                  address: address!,
+                                  phone: phone!,
+                                  email: email!,
+                                  password: password!,
+                                  typeUser: AppConstant.typeUsers[1],
+                                  geoPoint: GeoPoint(
+                                      position!.latitude, position!.longitude),
+                                  skillTechnic: skillTechnics,
+                                );
+                      
+                                AppService()
+                                    .processCreateNewAccount(
+                                        email: email!,
+                                        password: password!,
+                                        context: context,
+                                        userModel: userModel)
+                                    .then((value) {
+                                 
+                                });
+                              }
+                            }, // onpresss
+                          ),
+                          
+                        ],
+                      ),
                     ),
                   ),
                 ),
