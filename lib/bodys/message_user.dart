c4e4ac7b9c5349e7072c97e28959a02e5bdff5ba
ung <test@abc.com>
2023-01-30 11:55:29 +0700

    before lunch

--- lib/bodys/message_user.dart
+++ lib/bodys/message_user.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:technicservice/models/chat_model.dart';
+import 'package:technicservice/utility/app_controller.dart';
+import 'package:technicservice/widgets/widget_menu.dart';
 import 'package:technicservice/widgets/widget_text.dart';
 
-class MessageUser extends StatelessWidget {
+class MessageUser extends StatefulWidget {
   const MessageUser({super.key});
 
   @override
+  State<MessageUser> createState() => _MessageUserState();
+}
+
+class _MessageUserState extends State<MessageUser> {
+  AppController controller = Get.put(AppController());
+
+  @override
+  void initState() {
+    super.initState();
+    controller.readChatModelForUser();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return WidgetText(text: 'This is MessageUser');
+    return GetX(
+        init: AppController(),
+        builder: (AppController appController) {
+          print('##30jan chatModles ---> ${appController.chatModels}');
+          return appController.chatModels.isEmpty
+              ? const SizedBox()
+              : ListView.builder(
+                  itemCount: appController.chatModels.length,
+                  itemBuilder: (context, index) =>
+                     WidgetMenu(leadWidget: Icon(Icons.chat), title: 'title'),
+                );
+        });
   }
 }
